cb11aeac9a521766a89a933920dc4c5704a2e26e
Muhammed Ziyad <muhdziyadu@gmail.com>
2024-01-19 10:24:04 +0530

    add paramas to route

--- routes/router.js
+++ routes/router.js
@@ -2,6 +2,6 @@ const express = require('express')
 const router = express.Router()
 const controller = require('../controllers/controller')
 
-router.get('/fetchDetails',controller.getDetails)
+router.get('/fetchDetails/:vrNo',controller.getDetails)
 
 module.exports = router
